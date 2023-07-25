77271412cd48705a3a3833a838891195c962e1e3
Henry1four <140516962+Henry1four@users.noreply.github.com>
2023-07-25 07:54:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_3lr4g1o", "template_9gkivtw", formData)
+    emailjs.send("service_lpltlua", "template_c8c1yhi", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
